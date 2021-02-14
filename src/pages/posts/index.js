63170207e99d6bf551825d4c02a360692640e45c
rellyso <rellysondouglas2015@gmail.com>
2021-02-14 15:51:38 -0300

    getting updates and configuring init of project

---
+++ src/pages/posts/index.js
@@ -0,0 +1,66 @@
+import { makeStyles } from '@material-ui/core'
+import {
+    Create,
+    Edit,
+    List,
+    Datagrid,
+    SimpleForm,
+    TextField,
+    TextInput,
+    DateInput,
+    EditButton,
+    DateField,
+    DeleteButton,
+} from 'react-admin'
+
+// styles
+const useStyles = makeStyles({
+    headerRow: {
+        height: '40px'
+    },
+    row: {
+        height: '50px'
+    }
+})
+
+
+export const ListPosts = props => {
+    const classes = useStyles()
+
+    return (
+        <List {...props} title="Posts">
+            <Datagrid classes={{ rowCell: classes.row, headerRow: classes.headerRow }}>
+                <TextField source="id" />
+                <TextField label="Título" source="title" />
+                <DateField label="Publicado em" source="publishedAt" />
+                <EditButton basePath="/posts" />
+                <DeleteButton basePath="/posts" />
+            </Datagrid>
+        </List>
+    )
+}
+
+export const CreatePost = props => {
+    const classes = useStyles()
+
+    return (
+        <Create {...props} title="Novo Post" >
+            <SimpleForm>
+                <TextInput label="Título" source="title" />
+                <TextInput label="Conteúdo" source="body" multiline />
+                <DateInput label="Publicado em" source="publishedAt" />
+            </SimpleForm>
+        </Create>
+    )
+}
+
+export const EditPost = props => (
+    <Edit {...props} title="Editar Post">
+        <SimpleForm>
+            <TextInput disabled source="id" />
+            <TextInput label="Título" source="title" />
+            <TextInput label="Conteúdo" source="body" multiline />
+            <DateInput label="Publicado em" source="publishedAt" />
+        </SimpleForm>
+    </Edit>
+)
