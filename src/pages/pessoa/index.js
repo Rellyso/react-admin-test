5f87546977db3eb5c84834b8426258763ae73052
rellyso <rellysondouglas2015@gmail.com>
2021-02-15 23:19:42 -0300

    Added make styles and css to pessoa pages

--- src/pages/pessoa/index.js
+++ src/pages/pessoa/index.js
@@ -1,28 +1,53 @@
+import { makeStyles } from '@material-ui/core'
 import { SimpleForm, TextInput, TextField, Datagrid, Create, Edit, List } from 'react-admin'
 
-export const ListPessoa = props => (
-    <List {...props}>
-        <Datagrid rowClick="edit">
-            <TextField source="id"/>
-            <TextField source="nome"/>
-        </Datagrid>
-    </List>
-)
+// styles
+import './styles.css'
+const useStyles = makeStyles({
+    headerRow: {
+        height: '40px'
+    },
+    row: {
+        height: '50px'
+    },
+    editForm: {
+        color: 'red',
+    }
+})
 
-export const CreatePessoa = props => (
-    <Create {...props}>
-        <SimpleForm>
-            <TextInput source="id"/>
-            <TextInput source="nome"/>
-        </SimpleForm>
-    </Create>
-)
+export const ListPessoa = props => {
+    const classes = useStyles()
 
-export const EditPessoa = props => (
-    <Edit {...props}>
-        <SimpleForm>
-            <TextInput source="id"/>
-            <TextInput source="nome"/>
-        </SimpleForm>
-    </Edit>
-)
+    return (
+        <List {...props}>
+            <Datagrid rowClick="edit" classes={{ headerRow: classes.headerRow, rowCell: classes.row }}>
+                <TextField source="id" />
+                <TextField source="nome" />
+            </Datagrid>
+        </List>
+    )
+}
+
+export const CreatePessoa = props => {
+    const classes = useStyles()
+    return (
+        <Create {...props} classes={{ headerRow: classes.headerRow, rowCell: classes.row }}>
+            <SimpleForm>
+                <TextInput source="id" />
+                <TextInput source="nome" />
+            </SimpleForm>
+        </Create>
+    )
+}
+
+export const EditPessoa = props => {
+    const classes = useStyles()
+    return (
+        <Edit {...props} classes={{ headerRow: classes.headerRow, rowCell: classes.row }}>
+            <SimpleForm className={classes.editForm} fullWidth>
+                <TextInput source="id" />
+                <TextInput source="nome" />
+            </SimpleForm>
+        </Edit>
+    )
+}
