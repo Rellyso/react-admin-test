031af0384dd3d21ddbd9ec092e630e81ef45301c
rellyso <rellysondouglas2015@gmail.com>
2021-02-17 13:44:01 -0300

    passing admin to page Home

--- src/App.js
+++ src/App.js
@@ -1,46 +1,14 @@
-import { Admin, Resource } from 'react-admin'
-import jsonServerProvider from 'ra-data-json-server'
-import { makeStyles } from '@material-ui/core/styles'
-import authProvider from './providers/authProvider'
+import Home from './pages/Home'
 
-// pages
-import { CreatePessoa, EditPessoa, ListPessoa } from './pages/pessoa'
-import { CreateTipo, EditTipo, ListTipo } from './pages/tipo'
-import { CreatePost, ListPosts, EditPost } from './pages/posts'
-import Dashboard from './pages/Dashboard'
-import Login from './pages/Login'
-
-// components
-import Menu from './components/Menu'
-import MyLayout from './components/Layout'
-
-// icons
-import PersonIcon from '@material-ui/icons/Person';
-import BarChartIcon from '@material-ui/icons/BarChart';
-import LocalPostOfficeIcon from '@material-ui/icons/LocalPostOffice';
-
-// styles
-const useStyles = makeStyles({
-  resource: {
-    padding: '8px 16px',
-  }
-})
-
-const dataProvider = jsonServerProvider("http://localhost:5000")
+import { AuthProvider } from './Context/AuthContext'
 
 const App = () => {
-  const classes = useStyles()
-  
+
   return (
-    <Admin layout={MyLayout} dataProvider={dataProvider} authProvider={authProvider} menu={Menu} dashboard={Dashboard} loginPage={Login} >
-      <Resource className="resource" name="pessoa" list={ListPessoa} edit={EditPessoa} create={CreatePessoa} icon={PersonIcon} />
-
-      <Resource className={classes.resource} name="tipo" list={ListTipo} edit={EditTipo} create={CreateTipo} icon={BarChartIcon} />
-
-      <Resource className={classes.resource} name="posts" list={ListPosts} edit={EditPost} create={CreatePost} icon={LocalPostOfficeIcon} />
-
-    </Admin>
-)
-} 
+    <AuthProvider>
+      <Home />
+    </AuthProvider>
+  )
+}
 
 export default App
---
+++ src/pages/Home/index.js
@@ -0,0 +1,46 @@
+import { Admin, Resource } from 'react-admin'
+import jsonServerProvider from 'ra-data-json-server'
+import { makeStyles } from '@material-ui/core/styles'
+import authProvider from '../../providers/authProvider'
+
+// pages
+import { CreatePessoa, EditPessoa, ListPessoa } from '../pessoa'
+import { CreateTipo, EditTipo, ListTipo } from '../tipo'
+import { CreatePost, ListPosts, EditPost } from '../posts'
+import Dashboard from '../Dashboard'
+import Login from '../Login'
+
+// components
+import Menu from '../../components/Menu'
+import MyLayout from '../../components/Layout'
+
+// icons
+import PersonIcon from '@material-ui/icons/Person';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import LocalPostOfficeIcon from '@material-ui/icons/LocalPostOffice';
+
+// styles
+const useStyles = makeStyles({
+  resource: {
+    padding: '8px 16px',
+  }
+})
+
+const dataProvider = jsonServerProvider("http://localhost:5000")
+
+const Home = () => {
+  const classes = useStyles()
+  
+  return (
+    <Admin layout={MyLayout} dataProvider={dataProvider} authProvider={authProvider} menu={Menu} dashboard={Dashboard} loginPage={Login} >
+      <Resource className="resource" name="pessoa" list={ListPessoa} edit={EditPessoa} create={CreatePessoa} icon={PersonIcon} />
+
+      <Resource className={classes.resource} name="tipo" list={ListTipo} edit={EditTipo} create={CreateTipo} icon={BarChartIcon} />
+
+      <Resource className={classes.resource} name="posts" list={ListPosts} edit={EditPost} create={CreatePost} icon={LocalPostOfficeIcon} />
+
+    </Admin>
+)
+} 
+
+export default Home
