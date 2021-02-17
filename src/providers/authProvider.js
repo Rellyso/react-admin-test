66940aea90eef9676b51091b87bee75784892a4d
rellyso <rellysondouglas2015@gmail.com>
2021-02-16 22:02:07 -0300

    Getitng authProvider

---
+++ src/providers/authProvider.js
@@ -0,0 +1,32 @@
+export default {
+    login: async ({ username }) => {
+        localStorage.setItem('username', username);
+        // accept all username/password combinations
+
+        console.log(users[0].email)
+
+        // if (username === users[0].email)
+        return Promise.resolve();
+    },
+    // called when the user clicks on the logout button
+    logout: () => {
+        localStorage.removeItem('username');
+        return Promise.resolve();
+    },
+    // called when the API returns an error
+    checkError: ({ status }) => {
+        if (status === 401 || status === 403) {
+            localStorage.removeItem('username');
+            return Promise.reject();
+        }
+        return Promise.resolve();
+    },
+    // called when the user navigates to a new location, to check for authentication
+    checkAuth: () => {
+        return localStorage.getItem('username')
+            ? Promise.resolve()
+            : Promise.reject();
+    },
+    // called when the user navigates to a new location, to check for permissions / roles
+    getPermissions: () => Promise.resolve(),
+};
